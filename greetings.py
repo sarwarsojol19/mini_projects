7f08da5208e035d5917c9c605ae2367e4159a883
sarwarsojol19 <sarwarsojol@outlook.com>
2023-02-04 20:20:31 +0600

    1st mini project done

---
+++ greetings.py
@@ -0,0 +1,12 @@
+# Taking inputs from user
+
+fName =input("What is your First Name: ")
+lName =input("What is your Last Name: ")
+
+# join two variables and make title case
+
+fullName = fName.title() + " " + lName.title()
+
+#show  massage to user
+
+print(f"Your full name is {fullName}")
